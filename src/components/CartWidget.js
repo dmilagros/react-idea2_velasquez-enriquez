90eb8795f2890ae7d75513dded9485550ec50adb
dmilagros <dmilagrosvelasquez@gmail.com>
2022-08-28 01:59:49 -0500

    add greeting message

---
+++ src/components/CartWidget.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CartWidget = () => {
+  return (
+    <div>
+      <i class="small material-icons">shopping_cart</i>
+      
+    </div>
+  );
+};
+
+export default CartWidget;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,30 @@
 import React from "react";
-import './navBar.css';
+import CartWidget from "./CartWidget";
+import "./navBar.css";
 const NavBar = () => {
   return (
     <ul className="navigation">
-      <li><a href=""><img src="./logo.png" alt="" /></a></li>
+      <li>
+        <a href="">
+          <img src="./logo.png" alt="" />
+        </a>
+      </li>
       <ul className="navigation-items">
-        <li><a href="#"> Nosotros</a></li>
-        <li><a href="#"> Vehiculos</a></li>
-        <li><a href="#"> Electronica</a></li>
-        <li><a href="#"> Libros</a></li>
+        <li>
+          <a href="#">Nosotros</a>
+        </li>
+        <li>
+          <a href="#">Ropa</a>
+        </li>
+        <li>
+          <a href="#">Accesorios</a>
+        </li>
+        <li>
+          <a href="#">Contacto</a>
+        </li>
       </ul>
       <li className="navigation-button">
-        <button>Login</button>
+        <CartWidget />
       </li>
     </ul>
   );
